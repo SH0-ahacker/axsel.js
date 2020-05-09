e85b1a12377114ad2317f205f0a6470edbfebb3a
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2020-05-08 22:55:49 -0300

    Update .js
    open files (or other)

--- scripts/start (URL)/.js
+++ scripts/start (URL)/.js
@@ -1,3 +1,6 @@
-function start (URL) {
+cmd = "file:///C:/Windows/System32/cmd.exe"		// cmd.exe
+calc = "file:///C:/Windows/System32/calc.exe"		// calc.exe
+explorer = "file:///C:/Windows/System32/explorer.exe"	// explorer.exe
+function start(URL) {
 	window.open(URL)
 }
